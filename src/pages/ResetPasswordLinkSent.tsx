235e63b62f20026622876ea585fed3d3735276fc
Ayomidedavid56 <ayomidedavid56@yahoo.com>
2024-10-31 20:19:47 +0100

    add reset password flow

--- src/pages/ResetPasswordLinkSent.tsx
+++ src/pages/ResetPasswordLinkSent.tsx
@@ -1,89 +1,48 @@
 import LoginImg from "../assets/images/login_bg.png";
 import Logo from "../assets/images/city_logistics.png";
 import Button from "@/components/Button";
-import { useMutation } from "@tanstack/react-query";
-import { forgotPassword } from "@/apis/auth";
-import { useFormik } from "formik";
 import { useNavigate } from "@tanstack/react-router";
-import { dashboardRoute, indexRoute, ordersRoute, router } from "@/router";
-import * as yup from "yup";
-import { toast, Toaster } from "sonner";
-import { AxiosError } from "axios";
-import { parseError, validateAuth } from "@/lib/utils";
+import { dashboardRoute, indexRoute } from "@/router";
+import { Toaster } from "sonner";
+import { validateAuth } from "@/lib/utils";
 import { useEffect } from "react";
 
 export default function ResetPasswordLinkSent() {
   const navigate = useNavigate();
-
-  const { mutateAsync, isPending } = useMutation({
-    mutationFn: forgotPassword,
-    onSuccess: (_) => {
-      toast.success(
-        "An email containing instructions to reset your password has been sent to your email"
-      );
-      setTimeout(() => {
-        navigate({ to: indexRoute.to });
-      }, 3000);
-    },
-    onError: (e: AxiosError) => {
-      toast.error(parseError(e));
-    },
-  });
-
-  const validationSchema = yup.object().shape({
-    email: yup.string().email().required("this field is required"),
-  });
-
-  const initialValues = {
-    email: "",
-  };
-
-  const { handleSubmit, handleChange, values, errors, touched } = useFormik({
-    initialValues,
-    validationSchema,
-    onSubmit: (data) => {
-      console.info({ data });
-
-      mutateAsync({ email: data.email.toLowerCase() });
-    },
-  });
 
   useEffect(() => {
     if (validateAuth()) navigate({ to: dashboardRoute.to });
   }, []);
 
   return (
-    <form onSubmit={handleSubmit}>
-      <div
-        className=" bg-white h-screen w-screen bg-cover flex justify-center items-center"
-        style={{ backgroundImage: `url(${LoginImg})` }}
-      >
-        <Toaster richColors position="top-center" />
+    <div
+      className=" bg-white h-screen w-screen bg-cover flex justify-center items-center"
+      style={{ backgroundImage: `url(${LoginImg})` }}
+    >
+      <Toaster richColors position="top-center" />
 
-        <div className="bg-white w-[95vw]  md:w-[38rem] rounded-2xl flex flex-col items-center p-[1rem] md:p-[3.75rem]">
-          <img
-            src={Logo}
-            alt="City Logo"
-            className="w-[7.5rem] h-[3.75rem] bg-cover"
+      <div className="bg-white w-[95vw]  md:w-[38rem] rounded-2xl flex flex-col items-center p-[1rem] md:p-[3.75rem]">
+        <img
+          src={Logo}
+          alt="City Logo"
+          className="w-[7.5rem] h-[3.75rem] bg-cover"
+        />
+        <div className="text-[#202224] text-[2rem] font-bold font-['Nunito Sans']">
+          Reset Password Link Sent
+        </div>
+        <div className="opacity-80 text-[#202224] text-lg font-semibold font-['Nunito Sans']">
+          An email containing instructions to reset your password has been sent
+          to your email.
+        </div>
+
+        <div className="w-[min(25rem,100%)]  mt-[4.875rem]">
+          <Button
+            type="submit"
+            text="Continue"
+            onClick={() => navigate({ to: indexRoute.to })}
           />
-          <div className="text-[#202224] text-[2rem] font-bold font-['Nunito Sans']">
-            Reset Password Link Sent
-          </div>
-          <div className="opacity-80 text-[#202224] text-lg font-semibold font-['Nunito Sans']">
-            An email containing instructions to reset your password has been
-            sent to your email.
-          </div>
-
-          <div className="w-[min(25rem,100%)]  mt-[4.875rem]">
-            <Button
-              type="submit"
-              loading={isPending}
-              text="Continue"
-              onClick={() => navigate({ to: indexRoute.to })}
-            />
-          </div>
         </div>
       </div>
-    </form>
+    </div>
   );
 }
